70c8ba678f96c964e3ca04e2f569f960fc2f76f7
Forsy-official <nadezdadoktorova6@gmail.com>
2026-05-29 22:12:49 +0300

    Update maaaa.py

--- maaaa.py
+++ maaaa.py
@@ -4,12 +4,11 @@ from maw import get_info_patyorka
 from maw import get_info_lenta
 from maw import get_info_magnit
 
-
 def p():
     a = [
         "Хлеб белый",
-        "Молоко 3.2%",
-        "Яйца куриные (десяток)",
+        "Молоко",
+        "Яйца куриные",
         "Сахар-песок",
         "Соль поваренная",
         "Картофель",
@@ -23,64 +22,11 @@ def p():
         "Подсолнечное масло",
         "Чай чёрный"
     ]
-
     answer = []
     for i in a:
         try:
             answer.append(get_info_patyorka(i))
-        except Exception as e:
-            print(e)
-    return answer
-
-def l():
-    a = [
-        "Хлеб белый",
-        "Молоко 3.2%",
-        "Яйца куриные (десяток)",
-        "Сахар-песок",
-        "Соль поваренная",
-        "Картофель",
-        "Лук репчатый",
-        "Морковь",
-        "Яблоки",
-        "Бананы",
-        "Куриное филе",
-        "Гречневая крупа",
-        "Рис длиннозерный",
-        "Подсолнечное масло",
-        "Чай чёрный"
-    ]
-
-    answer = []
-    for i in a:
-        try:
             answer.append(get_info_lenta(i))
-        except Exception as e:
-            print(e)
-    return answer
-
-def m():
-    a = [
-        "Хлеб белый",
-        "Молоко 3.2%",
-        "Яйца куриные (десяток)",
-        "Сахар-песок",
-        "Соль поваренная",
-        "Картофель",
-        "Лук репчатый",
-        "Морковь",
-        "Яблоки",
-        "Бананы",
-        "Куриное филе",
-        "Гречневая крупа",
-        "Рис длиннозерный",
-        "Подсолнечное масло",
-        "Чай чёрный"
-    ]
-
-    answer = []
-    for i in a:
-        try:
             answer.append(get_info_magnit(i))
         except Exception as e:
             print(e)
@@ -88,12 +34,12 @@ def m():
 
 @app.route('/main')
 def main():
-    tovar = f()
+    tovar = p()
     return flask.render_template("main.html",text=tovar, filteres=filteres)
 
 
 def filteres(filter):
-    tovar = f()
+    tovar = p()
     ls = []
     for element in tovar:
         if element["shop"] == filter:
